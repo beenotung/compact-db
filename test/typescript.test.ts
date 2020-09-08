d9b253a3ff2bebfc1cd781b20a5b30d3b6c07455
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-09-08 16:06:41 +0800

    delete unrelated test file

--- test/typescript.test.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-describe('ts-jest setup', () => {
-  it('should be able to compile', () => {
-    expect(fs.existsSync('package.json')).toBeTruthy()
-    const text = fs.readFileSync('package.json').toString()
-    const json = JSON.parse(text)
-    expect(json).toBeDefined()
-  })
-})
