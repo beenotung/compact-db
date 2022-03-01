25df5646b96c05dfbfa2bfd2971eae5b5b0568cc
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-03-01 20:20:31 +0800

    remove deprecated dependencies
    - remove tslint;
    - replace jest with mocha;
    - upgrade @beenotung/tslib version;

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { createDB, DB } from '../src/db'
+import { expect } from 'chai'
 
 describe('DB TestSuit', function () {
   const dir = 'data'
@@ -13,23 +14,23 @@ describe('DB TestSuit', function () {
     db.set('escaped string', `I'm good`)
     db.set('user', { name: 'Alice', age: 20 })
 
-    expect(db.get('num')).toEqual(42)
-    expect(db.get('str')).toEqual('Hello world')
-    expect(db.get('escaped string')).toEqual(`I'm good`)
-    expect(db.get('user')).toEqual({ name: 'Alice', age: 20 })
+    expect(db.get('num')).to.equal(42)
+    expect(db.get('str')).to.equal('Hello world')
+    expect(db.get('escaped string')).to.equal(`I'm good`)
+    expect(db.get('user')).to.deep.equal({ name: 'Alice', age: 20 })
   })
   it('should delete values', function () {
     db.set('num', 42)
-    expect(db.get('num')).toEqual(42)
+    expect(db.get('num')).to.equal(42)
     db.del('num')
-    expect(db.get('num')).toBeUndefined()
+    expect(db.get('num')).to.be.undefined
   })
   it('should clear all values', function () {
     db.set(1, 1)
     db.set(2, 2)
     db.clear()
-    expect(db.get(1)).toBeUndefined()
-    expect(db.get(2)).toBeUndefined()
+    expect(db.get(1)).to.be.undefined
+    expect(db.get(2)).to.be.undefined
   })
   it('should load data of previous session', function () {
     for (let i = 0; i < 12; i++) {
@@ -37,7 +38,7 @@ describe('DB TestSuit', function () {
       const value = Math.random()
       db1.set('num', value)
       const db2 = createDB({ path: dir })
-      expect(db2.get('num')).toEqual(value)
+      expect(db2.get('num')).to.equal(value)
     }
   })
   it('should support batch update', function () {
@@ -49,24 +50,24 @@ describe('DB TestSuit', function () {
       { type: 'set', key: 4, value: 4 },
       { type: 'del', key: 4 },
     ])
-    expect(db.get(1)).toBeUndefined()
-    expect(db.get(2)).toBeUndefined()
-    expect(db.get(3)).toEqual(3)
-    expect(db.get(4)).toBeUndefined()
+    expect(db.get(1)).to.be.undefined
+    expect(db.get(2)).to.be.undefined
+    expect(db.get(3)).to.equal(3)
+    expect(db.get(4)).to.be.undefined
   })
   it('should get all keys', function () {
     db.clear()
     db.set('a', 1)
     db.set('b', 1)
     db.set('c', 1)
-    expect(db.keys()).toEqual(['a', 'b', 'c'])
+    expect(db.keys()).to.deep.equal(['a', 'b', 'c'])
   })
   it('should get all entries', function () {
     db.clear()
     db.set('a', 1)
     db.set('b', 2)
     db.set('c', 3)
-    expect(db.entries()).toEqual([
+    expect(db.entries()).to.deep.equal([
       ['a', 1],
       ['b', 2],
       ['c', 3],
@@ -77,6 +78,6 @@ describe('DB TestSuit', function () {
     db.set('a', 1)
     db.set('b', 2)
     db.set('c', 3)
-    expect(db.values()).toEqual([1, 2, 3])
+    expect(db.values()).to.deep.equal([1, 2, 3])
   })
 })
